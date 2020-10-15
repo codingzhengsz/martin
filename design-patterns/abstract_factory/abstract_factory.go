2f71b804ae993d9a237bb723aa1a2f9e825d5305
Martin <codingzhengsz@163.com>
2020-10-16 00:26:47 +0800

    [Martin] abstract factory

---
+++ design-patterns/abstract_factory/abstract_factory.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type iSportsFactory interface {
+	makeShoe() iShoe
+	makeShirt() iShirt
+}
+
+func getSportsFactory(brand string) (iSportsFactory, error) {
+	if brand == "adidas" {
+		return &adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &nike{}, nil
+	}
+
+	return nil, fmt.Errorf("Wrong brand type passed ")
+}
+
+type adidas struct {
+}
+
+func (a *adidas) makeShoe() iShoe {
+	return &adidasShoe{
+		shoe: shoe{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+func (a *adidas) makeShirt() iShirt {
+	return &adidasShirt{
+		shirt: shirt{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+type nike struct {
+}
+
+func (n *nike) makeShoe() iShoe {
+	return &nikeShoe{
+		shoe: shoe{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+func (n *nike) makeShirt() iShirt {
+	return &nikeShirt{
+		shirt: shirt{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
